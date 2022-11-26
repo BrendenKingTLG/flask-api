483922fe210fa3d0575524f96dac5bc5ab8014b1
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-11-25 22:29:36 -0600

    first attempt at deployment

--- app.py
+++ app.py
@@ -2,27 +2,52 @@ from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
+USER_VALIDATION = False
+
+# username validation
+def user_validation(userName):
+    if userName == "" or userName == None:
+        return make_response(render_template("error.html"))
+    else:
+        global USER_VALIDATION
+        USER_VALIDATION = True
+
+
+#login page sets cookie with username  
 @app.route('/setcookie', methods = ['POST', 'GET'])
 def setcookie():
     if request.method == 'POST':
         userName = request.form['username']
-        if userName == "" or userName == None:
-            return make_response(render_template("error.html"))
-    resp = make_response(render_template("success.html"))
-    resp.set_cookie('username', userName)
+        # username validation
+        user_validation(userName)
+        print(USER_VALIDATION)
+    if USER_VALIDATION == False:
+        return make_response(render_template("error.html"))
+    else:
+        resp = make_response(render_template("success.html"))
+        resp.set_cookie('username', userName)
     return resp
 
-@app.route('/profile')
+
+# home page
+@app.route('/home')
 def profile():
     userName = request.cookies.get('username')  
-    if userName == None or userName == "":
+    # username validation
+    print(USER_VALIDATION)
+    if USER_VALIDATION == False:
         return make_response(render_template("error.html"))
-    return render_template('profile.html', name=userName)
+    else:
+        return render_template('home.html', name=userName)
 
+
+#login page 
 @app.route('/')
 def login():
    return render_template('login.html')
 
+
+# list storing current characters
 characters = [
     "Anthony",
     "Brenden",
@@ -46,11 +71,15 @@ characters = [
     "Yaping",
     "Chad"
 ]
+
+
+# route for all characters
 @app.route("/characters/")
 def all():
     return render_template("/characters.html")
 
 
+# route to approprate character page
 @app.route("/characters/<name>")
 def generic(name):
     for character in characters:
@@ -60,5 +89,18 @@ def generic(name):
     else:
         return "That character does not exist"
 
+
+@app.route("/how-to")
+def how_to():
+    return render_template("how-to.html")
+
+
+# custom 404
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+
+# main
 if __name__ == "__main__":
 	app.run()
